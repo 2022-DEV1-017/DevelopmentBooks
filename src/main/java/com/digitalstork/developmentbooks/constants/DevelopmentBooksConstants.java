533de01c6bfad224cbf3099fe44d567fb9912511
Anonymous <>
2022-03-31 22:55:50 +0100

    Implement Failing Test : should apply 25% discount if 5 different books

--- src/main/java/com/digitalstork/developmentbooks/constants/DevelopmentBooksConstants.java
+++ src/main/java/com/digitalstork/developmentbooks/constants/DevelopmentBooksConstants.java
@@ -10,5 +10,6 @@ public abstract class DevelopmentBooksConstants {
     public static final Double TWO_DIFFERENT_BOOKS_DISCOUNT = 0.05;
     public static final Double THREE_DIFFERENT_BOOKS_DISCOUNT = 0.10;
     public static final Double FOUR_DIFFERENT_BOOKS_DISCOUNT = 0.20;
+    public static final Double FIVE_DIFFERENT_BOOKS_DISCOUNT = 0.25;
 
 }
--- src/test/java/com/digitalstork/developmentbooks/service/DevelopmentBooksServiceTest.java
+++ src/test/java/com/digitalstork/developmentbooks/service/DevelopmentBooksServiceTest.java
@@ -115,4 +115,31 @@ public class DevelopmentBooksServiceTest {
         assertEquals(FOUR_DIFFERENT_BOOKS_DISCOUNT, discount.getRate());
         assertEquals(BOOK_PRICE * (1 - discount.getRate()), discount.getUnitPrice());
     }
+
+    @Test
+    void should_apply_25_percent_discount_if_5_different_books() {
+        // Given
+        BasketDto basket = BasketDto.builder()
+                .bookQuantities(new HashMap<>() {{
+                    put("1", 1);
+                    put("2", 1);
+                    put("3", 1);
+                    put("4", 1);
+                    put("5", 1);
+                }})
+                .build();
+
+        // When
+        BasketPriceDto basketPrice = developmentBooksService.calculatePrice(basket);
+
+        // Assertions
+        assertNotNull(basketPrice);
+        assertNotNull(basketPrice.getDiscounts());
+        assertEquals(1, basketPrice.getDiscounts().size());
+
+        DiscountDto discount = basketPrice.getDiscounts().stream().findAny().orElseThrow();
+
+        assertEquals(FIVE_DIFFERENT_BOOKS_DISCOUNT, discount.getRate());
+        assertEquals(BOOK_PRICE * (1 - discount.getRate()), discount.getUnitPrice());
+    }
 }
