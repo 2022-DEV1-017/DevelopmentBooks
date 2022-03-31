498d4014361c87ebbcd34772d4b0dcc076d23d1b
Anonymous <>
2022-03-31 07:27:31 +0100

    Fix Failing Test : Should not apply discount if one copy only

--- src/main/java/com/digitalstork/developmentbooks/service/DevelopmentBooksService.java
+++ src/main/java/com/digitalstork/developmentbooks/service/DevelopmentBooksService.java
@@ -2,14 +2,29 @@ package com.digitalstork.developmentbooks.service;
 
 import com.digitalstork.developmentbooks.dto.BasketDto;
 import com.digitalstork.developmentbooks.dto.BasketPriceDto;
+import com.digitalstork.developmentbooks.dto.DiscountDto;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class DevelopmentBooksService implements IDevelopmentBooksService {
 
     @Override
     public BasketPriceDto calculatePrice(BasketDto basket) {
-        return null;
+
+        if (basket.getBookQuantities().size() == 1) {
+            DiscountDto discount = new DiscountDto();
+            discount.setRate(0F);
+            discount.setUnitPrice(50.0);
+
+            BasketPriceDto basketPrice = new BasketPriceDto();
+            basketPrice.setDiscounts(Collections.singleton(discount));
+
+            return basketPrice;
+        } else {
+            return null;
+        }
     }
 
 }
