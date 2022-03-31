7b95c26ea3089e9c42883e68d0f925052ca53bb5
Anonymous <>
2022-03-31 22:52:30 +0100

    Implement Failing Test : should apply 20% discount if 4 different books

--- src/main/java/com/digitalstork/developmentbooks/constants/DevelopmentBooksConstants.java
+++ src/main/java/com/digitalstork/developmentbooks/constants/DevelopmentBooksConstants.java
@@ -9,5 +9,6 @@ public abstract class DevelopmentBooksConstants {
     public static final Double NO_DISCOUNT = 0.0;
     public static final Double TWO_DIFFERENT_BOOKS_DISCOUNT = 0.05;
     public static final Double THREE_DIFFERENT_BOOKS_DISCOUNT = 0.10;
+    public static final Double FOUR_DIFFERENT_BOOKS_DISCOUNT = 0.20;
 
 }
