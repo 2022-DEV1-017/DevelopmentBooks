7466de8e0d89dbee7d9cb06eb0994a15bfa6cb4b
Anonymous <>
2022-03-31 07:27:16 +0100

    Implement Failing Test : Should not apply discount if one copy only

---
+++ src/test/java/com/digitalstork/developmentbooks/service/DevelopmentBooksServiceTest.java
@@ -0,0 +1,42 @@
+package com.digitalstork.developmentbooks.service;
+
+import com.digitalstork.developmentbooks.dto.BasketDto;
+import com.digitalstork.developmentbooks.dto.BasketPriceDto;
+import com.digitalstork.developmentbooks.dto.DiscountDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+public class DevelopmentBooksServiceTest {
+
+    @InjectMocks
+    private DevelopmentBooksService developmentBooksService;
+
+    @Test
+    void should_not_apply_discount_if_one_copy_only() {
+        // Given
+        BasketDto basket = BasketDto.builder()
+                .bookQuantities(new HashMap<>() {{
+                    put("1", 1);
+                }})
+                .build();
+
+        // When
+        BasketPriceDto basketPrice = developmentBooksService.calculatePrice(basket);
+
+        // Assertions
+        assertNotNull(basketPrice);
+        assertNotNull(basketPrice.getDiscounts());
+        assertEquals(1, basketPrice.getDiscounts().size());
+        DiscountDto discount = basketPrice.getDiscounts().stream().findAny().orElseThrow();
+        assertEquals(0, discount.getRate());
+        assertEquals(50.0, discount.getUnitPrice());
+    }
+}
