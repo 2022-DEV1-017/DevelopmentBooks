86d83dd7783cef50c1434742d1b7bce064a4960f
Anonymous <>
2022-04-01 13:17:26 +0100

    Fix Failing Test: Rest controller returns HttpStatus.NOT_FOUND for unavailable books

--- src/main/java/com/digitalstork/developmentbooks/exceptions/RestExceptionHandler.java
+++ src/main/java/com/digitalstork/developmentbooks/exceptions/RestExceptionHandler.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,4 +40,16 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(apiError, headers, status);
     }
 
+    @ExceptionHandler(UnavailableBookException.class)
+    protected ResponseEntity<Object> handleResourceNotFoundException(UnavailableBookException ex) {
+        ApiError apiError = ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND)
+                .errorCode(ErrorCode.RESOURCE_NOT_FOUND.name())
+                .message(ex.getMessage())
+                .subErrors(new ArrayList<>())
+                .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
 }
