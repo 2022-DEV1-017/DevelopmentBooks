45cb26b8d8a1f8b1950b824631bdbb504cc4173f
Anonymous <>
2022-04-01 10:59:51 +0100

    Fix Failing Test : should return book details from externalCode

--- src/main/java/com/digitalstork/developmentbooks/service/BooksService.java
+++ src/main/java/com/digitalstork/developmentbooks/service/BooksService.java
@@ -1,14 +1,25 @@
 package com.digitalstork.developmentbooks.service;
 
+import com.digitalstork.developmentbooks.domain.Book;
 import com.digitalstork.developmentbooks.dto.BookDto;
+import com.digitalstork.developmentbooks.mapper.BookMapper;
+import com.digitalstork.developmentbooks.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BooksService implements IBooksService {
 
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper = new BookMapper();
+
+    public BooksService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public BookDto getBook(String externalKey) {
-        return null;
+        Book book = bookRepository.findBookByExternalCode(externalKey).orElseThrow();
+        return bookMapper.apply(book);
     }
 
 }
